ce9e07ff817daad99e5e3c324d9da4c36f647612
Alfonso Jimenez <ajimenez@agros.tech>
2022-05-20 13:50:30 -0500

    feat: Sc using CID hashes

--- test/Health-test.js
+++ test/Health-test.js
@@ -11,14 +11,14 @@ describe("HealthControl", function () {
     console.log("Health Structure deployed", roles.address);   
 
     const role_org = ethers.utils.id("ROLE_DOCTOR")  
-    const doctor_cid = 'QmP3qbWY2iNhvtjtijzdU4AgVW3pzrUbxPbxA9VMhc2nzr'
+    const doctor_cid = '0x6df1dff781c6d4b003ffc9288c7e7f0a8a75244ac271035db35249bd25a6de95'
     const doctor_1 = await roles.connect(admin).addDoctor(doctor.address, doctor_cid);
     const info_doctor = await roles.connect(admin).hasRole(role_org,doctor.address);
     expect(info_doctor).to.equal(true);
     console.log("Doctor added")
 
     const role_user = ethers.utils.id("ROLE_USER")  
-    const user_cid = "QmP3qbWY2iNhvtjtijzdU4AgVW3pzrUbxPbxA9VMhc2nzr"
+    const user_cid = "0x6df1dff781c6d4b003ffc9288c7e7f0a8a75244ac271035db35249bd25a6de95"
     const user_1 = await roles.connect(admin).addUser(user1.address,user_cid);
     const info_user = await roles.connect(admin).hasRole(role_user,user1.address);
     expect(info_user).to.equal(true);
@@ -33,14 +33,14 @@ describe("HealthControl", function () {
     console.log("Health Structure deployed", roles.address);   
 
     const role_org = ethers.utils.id("ROLE_DOCTOR")  
-    const doctor_cid = 'QmP3qbWY2iNhvtjtijzdU4AgVW3pzrUbxPbxA9VMhc2nzr'
+    const doctor_cid = '0x6df1dff781c6d4b003ffc9288c7e7f0a8a75244ac271035db35249bd25a6de95'
     const doctor_1 = await roles.connect(admin).addDoctor(doctor.address, doctor_cid);
     const info_doctor = await roles.connect(admin).hasRole(role_org,doctor.address);
     expect(info_doctor).to.equal(true);
     console.log("Doctor added")
 
     const role_user = ethers.utils.id("ROLE_USER")  
-    const user_cid = "QmP3qbWY2iNhvtjtijzdU4AgVW3pzrUbxPbxA9VMhc2nzr"
+    const user_cid = "0x6df1dff781c6d4b003ffc9288c7e7f0a8a75244ac271035db35249bd25a6de95"
     const user_1 = await roles.connect(admin).addUser(user1.address,user_cid);
     const info_user = await roles.connect(admin).hasRole(role_user,user1.address);
     expect(info_user).to.equal(true);
@@ -54,7 +54,7 @@ describe("HealthControl", function () {
     
     const approve= await store.connect(user1).approveUser(doctor.address)
     console.log("pass1")
-    const history_cid = 'QmP3qbWY2iNhvtjtijzdU4AgVW3pzrUbxPbxA9VMhc2nzr'
+    const history_cid = '0x6df1dff781c6d4b003ffc9288c7e7f0a8a75244ac271035db35249bd25a6de95'
     const historial = await store.connect(doctor).addHistorial(user1.address,history_cid)
     console.log("pass2")
     const view_history = await store.connect(doctor).viewHistorial(user1.address)
